1e3c3a63bbb39cf5fa25a6eac9a25d2ca2652b20
HETTski <tanski.hubo@gmail.com>
2024-09-28 23:38:14 +0200

    feat: updated header in home

--- win-your-life-mobile/app/(tabs)/index.tsx
+++ win-your-life-mobile/app/(tabs)/index.tsx
@@ -1,50 +1,47 @@
-import { Image, StyleSheet, Platform } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { StyleSheet, ScrollView, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { Collapsible } from "@/components/Collapsible";
-import { Ionicons } from "@expo/vector-icons";
-import {
-  Checkbox,
-  CheckboxIcon,
-  CheckboxIndicator,
-  CheckboxLabel,
-} from "@/components/ui/checkbox";
 import CheckBoxList from "@/components/CheckBoxList";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#343131" }}
-      headerImage={<Ionicons size={310} name="home" style={styles.reactLogo} />}
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Today's goals</ThemedText>
-      </ThemedView>
-      <CheckBoxList />
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.stickyHeader}>
+        <ThemedText type="title">Your streak is 10 days</ThemedText>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Today's goals</ThemedText>
+        </ThemedView>
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <CheckBoxList />
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#343131",
+  },
+  stickyHeader: {
+    width: '100%',
+    padding: 32,
+    backgroundColor: "#343131",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    zIndex: 1,
+  },
+  scrollViewContent: {
+    paddingTop: 32, // Adjust this value to ensure proper spacing under the sticky header
+    marginTop: 220, // Adding top margin to create space before CheckBoxList
+  },
   titleContainer: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
-
-    marginBottom: 12,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 250,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+    gap: 1,
+    padding: 16,
   },
 });

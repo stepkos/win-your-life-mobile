89c97e73033f6f42b5e268f887ab12c837938096
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-28 20:56:06 +0200

    feat: remove tab titles

--- win-your-life-mobile/app/(tabs)/_layout.tsx
+++ win-your-life-mobile/app/(tabs)/_layout.tsx
@@ -4,64 +4,68 @@ import React from "react";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Ionicons } from "@expo/vector-icons";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-  const size = 25;
+  const iconSize = 30;
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-        tabBarStyle: {},
+        tabBarStyle: styles.tabBar,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <View>
-              <Ionicons
-                name={focused ? "home" : "home-outline"}
-                color={color}
-                size={size}
-              />
-            </View>
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={iconSize}
+            />
           ),
+          tabBarLabel: () => null,
         }}
       />
       <Tabs.Screen
         name="friends"
         options={{
-          title: "Friends",
           tabBarIcon: ({ color, focused }) => (
-            <View>
-              <Ionicons
-                name={focused ? "people" : "people-outline"}
-                color={color}
-                size={size}
-              />
-            </View>
+            <Ionicons
+              name={focused ? "people" : "people-outline"}
+              color={color}
+              size={iconSize}
+            />
           ),
+          tabBarLabel: () => null,
         }}
       />
       <Tabs.Screen
         name="account"
         options={{
-          title: "Account",
           tabBarIcon: ({ color, focused }) => (
-            <View>
-              <Ionicons
-                name={focused ? "person-circle" : "person-circle-outline"}
-                color={color}
-                size={size}
-              />
-            </View>
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              color={color}
+              size={iconSize}
+            />
           ),
+          tabBarLabel: () => null,
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    // backgroundColor: "white",
+    paddingBottom: 10,
+    paddingTop: 10,
+    height: 70,
+    borderTopColor: "#f0f0f0",
+  },
+});

6c62f73bb74123f17d689838e0802bead22913cd
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-09-28 22:20:02 +0200

    fix

--- win-your-life-mobile/components/CheckBoxList.tsx
+++ win-your-life-mobile/components/CheckBoxList.tsx
@@ -3,7 +3,23 @@ import { Checkbox, CheckboxIndicator, CheckboxLabel } from "./ui/checkbox";
 import { View } from "react-native";
 import { ThemedText } from "./ThemedText";
 
-const data = ["medytacja", "2 litry wody", "basen", "fiszki", "20 min nauki"];
+const data = [
+  "Drink a glass of water in the morning",
+  "Read for 30 minutes",
+  "Exercise for 20 minutes",
+  "Meditate for 10 minutes",
+  "Write in a journal",
+  "Plan your day",
+  "Take a walk",
+  "Eat a healthy breakfast",
+  "Practice gratitude",
+  "Learn a new skill",
+  "Limit social media use",
+  "Get 8 hours of sleep",
+  "Declutter your space",
+  "Connect with a friend",
+  "Practice deep breathing",
+];
 
 export default function CheckBoxList() {
   return (
@@ -18,16 +34,16 @@ export default function CheckBoxList() {
             width: "100%", // Custom width
             height: 50, // Custom height
             borderRadius: 10,
-            backgroundColor: "#D8A25E"
+            borderColor: "#D8A25E",
+            borderWidth: 2,
+            padding: 10,
           }}
-
         >
-          <CheckboxIndicator 
-          >
+          <CheckboxIndicator>
             <Ionicons name="checkmark-sharp" />
           </CheckboxIndicator>
 
-          <CheckboxLabel style={{ color: "#410B0B", }}>
+          <CheckboxLabel style={{ color: "#410B0B" }}>
             <ThemedText>{item}</ThemedText>
           </CheckboxLabel>
         </Checkbox>
